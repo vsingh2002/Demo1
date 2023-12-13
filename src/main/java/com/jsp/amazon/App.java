36780c71aa28200dd4de149290c6da8b42033e2b
vsingh2002 <vishalkumarsinghv60@gmail.com>
2023-12-13 14:21:37 +0530

    hello

--- src/main/java/com/jsp/amazon/App.java
+++ src/main/java/com/jsp/amazon/App.java
@@ -9,6 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        
+        
         //Hello Vishal
         
     }
